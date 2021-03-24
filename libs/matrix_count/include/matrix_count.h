086ae5a7ca5c8a4cf6c2b6d99c3640b5ebb0d495
Alexey Kosenko <alexey.kosenko@mail.ru>
2021-03-25 01:54:25 +0300

    Add shared lib with all funcs => refactor

--- libs/matrix_count/include/matrix_count.h
+++ libs/matrix_count/include/matrix_count.h
@@ -12,8 +12,10 @@ typedef struct Matrix {
 } Matrix;
 
 Matrix *create_matrix(size_t rows, size_t cols);
+Matrix *create_matrix_from_file(FILE *ptr);
 int print_matrix(Matrix *mat, FILE *ptr);
 int cols_sum(Matrix *mat);
 int print_cols_sum(Matrix *mat, FILE *ptr);
+int delete_matrix(Matrix *mat);
 
 #endif // IW_2_LIBS_STATIC_INCLUDE_MATRIX_COUNT_H_
--- main_forked.c
+++ main_forked.c
@@ -1,27 +1,34 @@
 #include "matrix_count_forked.h"
 
-int main() {
-    FILE *ptr = fopen("../matrix_data/matrix_forked.txt", "w+");
+#include <time.h>
 
-    Matrix *mat = create_matrix(10, 5);
+int main() {
+    double start_time =  clock();
+
+    FILE *ptr = fopen("../matrix_data/matrix.txt", "r");
+    FILE *ptr_2 = fopen("../matrix_data/matrix_forked.txt", "w+");
+
+    Matrix *mat = create_matrix_from_file(ptr);
     if (mat == NULL) {
         return -1;
     }
-
-    if (print_matrix(mat, ptr) == -1) {
+    if (print_matrix(mat, ptr_2) == -1) {
         return -1;
     }
-    fprintf(ptr, "\n");
+    fflush(ptr_2);
 
-//    if (cols_sum(mat) == -1) {
-//        return -1;
-//    }
-//
-//    if (print_cols_sum(mat, ptr) == -1) {
-//        return -1;
-//    }
+    if (cols_sum(mat) == -1) {
+        return -1;
+    }
+    if (print_cols_sum(mat, ptr_2) == -1) {
+        return -1;
+    }
 
     fclose(ptr);
+    delete_matrix(mat);
 
+    double end_time = clock();
+    double  search_time = (double)(end_time - start_time) / CLOCKS_PER_SEC;
+    printf("time: %2.2f", search_time);
     return 0;
 }
