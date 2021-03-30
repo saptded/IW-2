18167374c3cb724b0eacc5945ef9c9917b0f672d
Alexey Kosenko <alexey.kosenko@mail.ru>
2021-03-31 00:56:45 +0300

    mb finished

--- libs/matrix_count_forked/include/matrix_count_forked.h
+++ libs/matrix_count_forked/include/matrix_count_forked.h
@@ -1,15 +1,16 @@
 #ifndef IW_2_LIBS_MATRIX_COUNT_FORKED_INCLUDE_MATRIX_COUNT_FORKED_H_
 #define IW_2_LIBS_MATRIX_COUNT_FORKED_INCLUDE_MATRIX_COUNT_FORKED_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/mman.h>
 #include <sys/wait.h>
 #include <sys/msg.h>
-#include <stdatomic.h>
-#include <sys/ipc.h>
-#include <sys/types.h>
 
 
 typedef struct Matrix {
@@ -20,10 +21,14 @@ typedef struct Matrix {
 } Matrix;
 
 Matrix *create_matrix(size_t rows, size_t cols);
-Matrix *create_matrix_from_file(FILE *ptr);
-int print_matrix(Matrix *mat, FILE *ptr);
+Matrix *create_matrix_from_file(const char *file_path);
+int print_matrix(const Matrix *mat, const char *file_path);
 int cols_sum(Matrix *mat);
-int print_cols_sum(Matrix *mat, FILE *ptr);
+int print_cols_sum(const Matrix *mat, const char *file_path);
 int delete_matrix(Matrix *mat);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // IW_2_LIBS_MATRIX_COUNT_FORKED_INCLUDE_MATRIX_COUNT_FORKED_H_
